8d786f3d34b1083974fce4acbd1a7f69be1ca884
Luke Ramsden <hello@lukeramsden.com>
2023-08-07 07:44:14 +0100

    Add withdrawal service interface and stub from task

---
+++ src/main/java/com/lukeramsden/boku/WithdrawalService.java
@@ -0,0 +1,30 @@
+package com.lukeramsden.boku;
+
+import java.util.UUID;
+
+interface WithdrawalService {
+    /**
+     * Request a withdrawal for given address and amount. Completes at random moment between 1 and 10 seconds
+     * @param id - a caller generated withdrawal id, used for idempotency
+     * @param address - an address withdraw to, can be any arbitrary string
+     * @param amount - an amount to withdraw
+     * @throws IllegalArgumentException in case there's different address or amount for given id
+     */
+    void requestWithdrawal(WithdrawalId id, Address address, Amount amount);
+
+    /**
+     * Return current state of withdrawal
+     * @param id - a withdrawal id
+     * @return current state of withdrawal
+     * @throws IllegalArgumentException in case there no withdrawal for the given id
+     */
+    WithdrawalState getRequestState(WithdrawalId id);
+
+    enum WithdrawalState {
+        PROCESSING, COMPLETED, FAILED
+    }
+
+    record Amount(long scale, int exponent) {}
+    record WithdrawalId(UUID value) {}
+    record Address(String value) {}
+}
