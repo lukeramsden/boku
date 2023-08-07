6067999004f01132a36f3ea25eef147c9ca8f701
Luke Ramsden <hello@lukeramsden.com>
2023-08-07 14:52:52 +0100

    Add empty validation test

---
+++ src/test/java/com/lukeramsden/boku/integrationtest/ValidationIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.lukeramsden.boku.integrationtest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class ValidationIntegrationTest
+{
+    @Test
+    @Disabled("WIP")
+    void shouldReject()
+    {
+        // TODO: parameterised test for invalid body payloads
+    }
+}
